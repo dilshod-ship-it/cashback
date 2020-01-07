44a741c6a6b32df2aca8fa357473056dfd26fd0f
Student <student@localhost>
2020-01-07 10:13:27 +0500

    Initial version

---
+++ src/js/app.js
@@ -0,0 +1,6 @@
+const regularPurchases = 2000;
+//0.01 - magic value (magic constant, hardcoded)
+//const cahsback = regularPurchases * 0.01 - так делать не нужно!
+const regularPurchasesPercent = 0.01;
+const cashback = regularPurchases * regularPurchasesPercent;
+console.log(cashback);
