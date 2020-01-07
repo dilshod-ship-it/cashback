3c1c88246f920f3ac9070aed2cf95f215682b7e9
Student <student@localhost>
2020-01-07 10:56:35 +0500

    Cashback limit add

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,9 @@
-const regularPurchases = 2000;
+const regularPurchases = 3000000;
 //0.01 - magic value (magic constant, hardcoded)
 //const cahsback = regularPurchases * 0.01 - так делать не нужно!
 const regularPurchasesPercent = 0.01;
-const cashback = regularPurchases * regularPurchasesPercent;
+let cashback = regularPurchases * regularPurchasesPercent;
+const cashbackLimit = 3000;
+if (cashback > cashbackLimit)
+{ cashback = cashbackLimit; }
 console.log(cashback);
