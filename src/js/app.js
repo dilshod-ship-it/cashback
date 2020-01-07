f8e30f83ecf732d1c60f73862e4d451dc9d739c7
Student <student@localhost>
2020-01-07 11:07:15 +0500

    Increased and special cashback add

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,18 @@
-const regularPurchases = 3000000;
 //0.01 - magic value (magic constant, hardcoded)
 //const cahsback = regularPurchases * 0.01 - так делать не нужно!
+const regularPurchases = 100;
 const regularPurchasesPercent = 0.01;
-let cashback = regularPurchases * regularPurchasesPercent;
+
+const increasePurchases = 100;
+const increasePurchasesPercent = 0.05;
+
+const specialPurchases = 100;
+const specialPurchasesPercent = 0.3;
+
+let cashback = regularPurchases * regularPurchasesPercent + 
+	increasePurchases * increasePurchasesPercent + 
+	specialPurchases * specialPurchasesPercent;
+
 const cashbackLimit = 3000;
 if (cashback > cashbackLimit)
 { cashback = cashbackLimit; }
